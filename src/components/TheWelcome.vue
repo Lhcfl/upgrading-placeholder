504727eaa3f22e1fbf49a4b468b3fd8ae1a900ed
Lhcfl <lhcfl@outlook.com>
2025-03-03 11:33:28 +0800

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>喵</template>
+      喵喵喵，喵。
+  </WelcomeItem>
+</template>
